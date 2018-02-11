458cbe38947be81a0fa8e429417d18cde5ddf326
cyril3 <zblinuxfun@gmail.com>
2018-02-11 14:11:42 +0800

    add some infomations

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',  # Required
+    version='0.1.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -90,6 +90,10 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Environment :: Console',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+
     ],
 
     # This field adds keywords for your project which will appear on the
